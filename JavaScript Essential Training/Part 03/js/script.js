08a247ffc7ce0f98299c4968baa99330410494ce
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2020-07-21 13:03:53 +0600

    Here have first 3 class code

---
+++ JavaScript Essential Training/Part 03/js/script.js
@@ -0,0 +1,29 @@
+// JavaScript Literals
+document.getElementById('demo').innerHTML = 50;
+
+// JavaScript Variables 
+var x;
+x = 10;
+x = 100;
+document.getElementById('demo').innerHTML = x;
+
+// JavaScript Operators 
+var x, y;
+// x = 10;
+x = 100;
+y = 50;
+// z = x + y;
+// z = x - y;
+// z = x * y;
+z = x / y;
+document.getElementById('demo').innerHTML = z;
+
+// JavaScript is case sansitive
+var lastName, lastname; // They are not same
+
+
+// This is single line comment
+
+/*
+This is multiple comment
+*/
