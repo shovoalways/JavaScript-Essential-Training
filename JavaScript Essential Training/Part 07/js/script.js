77986b2c1f51ac9121f4bca4138d48d7d8f47eb7
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2020-08-25 10:13:27 +0600

    JavaScript Operator precedence

---
+++ JavaScript Essential Training/Part 07/js/script.js
@@ -0,0 +1,28 @@
+/*
+JavaScript Operator Precedence
+*/
+
+let now = 2020;
+let yearAli = 1990;
+let fullAge = 21;
+
+
+let isFullAge = now - yearAli >= fullAge;
+
+let aliAge = now - yearAli;
+let shovoAge = 32;
+let average = (aliAge + shovoAge) / 2;
+
+
+document.getElementById('demo').innerHTML = isFullAge;
+
+
+console.log(average);
+
+let x;
+x = (10 + 5) * 15 - 16;
+console.log(x);
+
+
+
+// Reference https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
