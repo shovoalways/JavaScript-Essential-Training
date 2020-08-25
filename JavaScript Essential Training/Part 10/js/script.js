4972ab3395032e73be21c613052d07894c98dc18
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2020-08-25 10:46:00 +0600

    JavaScript Math

---
+++ JavaScript Essential Training/Part 10/js/script.js
@@ -0,0 +1,15 @@
+/*
+JavaScript Math
+*/
+
+console.log(Math.PI); // PI Value 3.141592653589793
+console.log(Math.round(4.499999)); // Round Value 4
+console.log(Math.pow(3, 2)); // Power Value 3
+console.log(Math.sqrt(144)); // Square Root Value 144
+console.log(Math.abs(-9)); // Always positive
+console.log(Math.ceil(4.1111)); // Maximum Value
+console.log(Math.floor(4.1111)); // Lowest Value
+console.log(Math.sin(90 * Math.PI / 180)); // Sin 90 degree = 1
+console.log(Math.max(0, 5, 8, 9, 55)); // Highest Value
+console.log(Math.min(0, 5, 8, 9, 55)); // Lowest Value
+console.log(Math.random()); // Random Number  
