e282462978ead3db029e4395e436b44376daf1bf
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2020-08-25 12:20:32 +0600

    JavaScript While Loop

---
+++ JavaScript Essential Training/Part 16/js/script.js
@@ -0,0 +1,10 @@
+/*
+JavaScript While Loop
+*/
+
+let num = 1;
+while (num < 10) {
+  console.log('Your Number is less then 10', num);
+  num++
+}
+document.write('<p>Your number is now over 10</p>');
